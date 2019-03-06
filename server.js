a2bc0c86649c75eea293529c1d8b7412534255a7
Attileos86 <n.a.maxwell86@gmail.com>
2019-03-06 19:21:57 +0100

    Initial commit

---
+++ server.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+const app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+hbs.registerHelper('getCurrentYear', () => new Date().getFullYear());
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+    const now = new Date().toString();
+    const log = `${now}: ${req.method} ${req.url}`;
+    fs.appendFile('logs.txt', log + '\n', (err) => {
+        if(err){
+            console.log('Unable to append to server.log');
+        }
+        next();
+    });
+    
+});
+
+app.use((req, res, next) => {
+    //return res.render('maintenance.hbs');
+    next();   
+});
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) =>{
+    console.log('Getting: /');
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        welcomeMessage: 'Welcome to my homepage',
+    });
+});
+
+app.get('/about', (req, res) => {
+    console.log('Getting: /about');
+    res.render('about.hbs', {
+        pageTitle: 'About page',
+    });
+});
+
+app.get('/bad', (req, res) => {
+    res.send({erroMessage: 'Unable to handle request'});
+});
+
+app.listen(3000, () => console.log('Server is up on port 3000'));
